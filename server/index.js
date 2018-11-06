9c9f19d2172924c4363495ef0a575488f5c372be
aleksy.dziarmaga@sap.com <aleksy.dziarmaga@gmail.com>
2018-11-06 11:54:56 +0100

    Add rsi endpoint

--- server/index.js
+++ server/index.js
@@ -20,6 +20,40 @@ app.get('/currencies/:baseCurrency', async(req, res) => {
         console.error(err);
         res.send(err);
     }
-})
+});
+
+app.get('/rsi', async(req, res) => {
+    const DAYS = 24;
+    try {
+        const { data } = await axios('http://api.nbp.pl/api/exchangerates/rates/a/USD/2018-10-01/2018-11-06/?format=json');
+        const midRates = data.rates.map( ({mid}) => mid);
+        const losses = [];
+        const gains = [];
+        let sumGains = 0;
+        let sumLosses = 0;
+        for (let i = 0; i < midRates.length - 1; i++) {
+            const change = midRates[i+1] - midRates[i];
+            if (change > 0) {
+                gains.push(change);
+                sumGains += change;
+            } else if (change < 0) {
+                losses.push(change)
+                sumLosses += change * -1
+            }
+        }
+        const avgGains = sumGains/midRates.length;
+        const avgLosses = sumLosses/midRates.length;
+        const rs = avgGains/avgLosses;
+        const rsi = 100 - (100/(1+rs));
+        const json = {
+            rsi: rsi,
+        }
+        res.json(rsi);
+
+    } catch(err) {
+        console.error(err);
+        res.send(err);
+    }
+});
 
 app.listen(PORT, () => console.log(`Currency service listening on port http://localhost:${PORT}`));
